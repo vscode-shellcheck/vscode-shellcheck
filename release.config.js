ab76fb060490a8209857d34040f79b5dde00edc5
Felipe Santos <felipecassiors@gmail.com>
2020-08-27 20:32:43 -0300

    chore: move to GitHub Actions and use semantic-release

---
+++ release.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    plugins: [
+        [
+            "@semantic-release/commit-analyzer",
+            {
+                releaseRules: [
+                    {
+                        type: "refactor",
+                        release: "patch",
+                    },
+                    {
+                        type: "build",
+                        scope: "deps",
+                        release: "patch",
+                    },
+                ],
+            },
+        ],
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/changelog",
+        ["@felipecrs/semantic-release-vsce", {
+            packageVsix: true,
+            yarn: true
+        }],
+        "@semantic-release/git",
+        [
+            "@semantic-release/github",
+            {
+                assets: "*.vsix",
+            },
+        ],
+    ],
+    preset: "conventionalcommits",
+};
