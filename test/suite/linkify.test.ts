cbd31d8a9541d62ce4e04e2f9b903f607f178770
Timon Wong <timon86.wang@gmail.com>
2022-04-01 00:41:23 +0800

    chore: add tests for link provider
    Signed-off-by: Timon Wong <timon86.wang@gmail.com>

---
+++ test/suite/linkify.test.ts
@@ -0,0 +1,39 @@
+import * as assert from "assert";
+import * as vscode from "vscode";
+import { LinkifyProvider } from "../../src/linkify";
+
+suite("Document Link Provider", () => {
+  test("Extension should linkify shellcheck directives automatically", async () => {
+    const linker = new LinkifyProvider();
+    const document = await vscode.workspace.openTextDocument({
+      content: `#!/bin/bash
+
+# shellcheck disable=SC1010
+echo $SHELL
+
+ #  shellcheck disable=SC1011
+echo $SHELL
+
+ #   shellcheck   disable=SC1012
+eval \`uname -r\`
+
+# shellcheck disable=SC1013
+`,
+      language: "shellscript",
+    });
+
+    await vscode.window.showTextDocument(document);
+
+    const links: vscode.ProviderResult<vscode.DocumentLink[]> =
+      await linker.provideDocumentLinks(document, {
+        isCancellationRequested: false,
+      } as vscode.CancellationToken);
+
+    assert.ok(links);
+    assert.strictEqual(links.length, 4);
+    for (const [i, link] of links.entries()) {
+      assert.strictEqual(link.target?.authority, "www.shellcheck.net");
+      assert.strictEqual(link.target?.path, `/wiki/SC101${i}`);
+    }
+  });
+});
