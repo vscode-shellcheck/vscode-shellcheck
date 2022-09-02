92d7a592b5fc931589bfd41e23295a5b04513da5
Timon Wong <timon86.wang@gmail.com>
2022-09-02 16:20:09 +0800

    feat: add prebuilt shellcheck binary for apple m1 (from homebrew)
    Signed-off-by: Timon Wong <timon86.wang@gmail.com>

--- bindl.config.js
+++ bindl.config.js
@@ -1,12 +1,14 @@
 const version = "v0.8.0";
-const commonUrl = `https://github.com/koalaman/shellcheck/releases/download/${version}/shellcheck-${version}`;
+const shortVersion = version.substring(1);
+const shellcheckReleaseUrl = `https://github.com/koalaman/shellcheck/releases/download/${version}/shellcheck-${version}`;
+const shellcheckM1DownloadBaseUrl = `https://github.com/vscode-shellcheck/shellcheck-m1/releases/download/${version}/`;
 
 module.exports = {
   binaries: [
     {
       platform: "linux",
       arch: "x64",
-      url: `${commonUrl}.linux.x86_64.tar.xz`,
+      url: `${shellcheckReleaseUrl}.linux.x86_64.tar.xz`,
       files: [
         {
           source: `shellcheck-${version}/shellcheck`,
@@ -17,7 +19,7 @@ module.exports = {
     {
       platform: "linux",
       arch: "arm",
-      url: `${commonUrl}.linux.armv6hf.tar.xz`,
+      url: `${shellcheckReleaseUrl}.linux.armv6hf.tar.xz`,
       files: [
         {
           source: `shellcheck-${version}/shellcheck`,
@@ -28,7 +30,7 @@ module.exports = {
     {
       platform: "linux",
       arch: "arm64",
-      url: `${commonUrl}.linux.aarch64.tar.xz`,
+      url: `${shellcheckReleaseUrl}.linux.aarch64.tar.xz`,
       files: [
         {
           source: `shellcheck-${version}/shellcheck`,
@@ -39,7 +41,7 @@ module.exports = {
     {
       platform: "darwin",
       arch: "x64",
-      url: `${commonUrl}.darwin.x86_64.tar.xz`,
+      url: `${shellcheckReleaseUrl}.darwin.x86_64.tar.xz`,
       files: [
         {
           source: `shellcheck-${version}/shellcheck`,
@@ -48,9 +50,21 @@ module.exports = {
       ],
     },
     {
+      platform: "darwin",
+      arch: "arm64",
+      // Sync from homebrew
+      url: `${shellcheckM1DownloadBaseUrl}shellcheck-${shortVersion}.tar.gz`,
+      files: [
+        {
+          source: `shellcheck/${shortVersion}/bin/shellcheck`,
+          target: "shellcheck",
+        },
+      ],
+    },
+    {
       platform: "win32",
       arch: "x32",
-      url: `${commonUrl}.zip`,
+      url: `${shellcheckReleaseUrl}.zip`,
       files: [{ source: `shellcheck.exe`, target: "shellcheck.exe" }],
     },
   ],
