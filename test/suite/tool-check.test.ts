19912932055009fecdce5dc7aca9a6fa18256e77
Timon Wong <timon86.wang@gmail.com>
2022-10-20 13:20:41 +0800

    chore: use string lib from es2020 (#777)

---
+++ test/suite/tool-check.test.ts
@@ -0,0 +1,26 @@
+import * as assert from "node:assert";
+import { parseToolVersion } from "../../src/utils/tool-check";
+
+suite("Tool Check Utils", () => {
+  test("parseToolVersion should work", async () => {
+    const stdout = `ShellCheck - shell script analysis tool
+version: 0.8.0
+license: GNU General Public License, version 3
+website: https://www.shellcheck.net
+`;
+
+    const version = parseToolVersion(stdout);
+    assert.strictEqual(version.format(), "0.8.0");
+  });
+
+  test("parseToolVersion should throw on invalid input", async () => {
+    const stdout = `ShellCheck - shell script analysis tool
+version: 0.8.a
+`;
+
+    assert.throws(
+      () => parseToolVersion(stdout),
+      "Unexpected response from ShellCheck"
+    );
+  });
+});
