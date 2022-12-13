0c1d46fdd027af3812666792be3a504c6a13ddc5
Felipe Santos <felipecassiors@gmail.com>
2022-12-13 13:30:00 -0300

    build(deps): upgrade bundled shellcheck from 0.8.0 to 0.9.0

--- test/suite/tool-check.test.ts
+++ test/suite/tool-check.test.ts
@@ -4,13 +4,13 @@ import { parseToolVersion } from "../../src/utils/tool-check";
 suite("Tool Check Utils", () => {
   test("parseToolVersion should work", async () => {
     const stdout = `ShellCheck - shell script analysis tool
-version: 0.8.0
+version: 0.9.0
 license: GNU General Public License, version 3
 website: https://www.shellcheck.net
 `;
 
     const version = parseToolVersion(stdout);
-    assert.strictEqual(version.format(), "0.8.0");
+    assert.strictEqual(version.format(), "0.9.0");
   });
 
   test("parseToolVersion should throw on invalid input", async () => {
