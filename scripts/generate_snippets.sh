189c4ef7393aa403ae36e46fab08386c984c4444
EmilySeville7cfg <EmilySeville7cfg@gmail.com>
2023-08-19 10:18:46 +1000

    feat(script): generate snippets

---
+++ scripts/generate_snippets.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+generate_snippets() {
+  gs_list_url='https://gist.githubusercontent.com/nicerobot/53cee11ee0abbdc997661e65b348f375/raw/d5a97b3b18ead38f323593532050f0711084acf1/_shellcheck.md'
+
+  wget -O - "$gs_list_url" 2> /dev/null |
+    jq -R '.' |
+    sed '1 s/"/["/; $! s/$/,/; $ s/"$/"]/' |
+    jq 'with_entries(
+        {
+          key: .value | capture("^- +\\[(?<x>SC\\d{4})\\]") | .x | ascii_downcase,
+          value:
+            {
+              prefix: .value | capture("^- +\\[(?<x>SC\\d{4})\\]") | .x | ascii_downcase,
+              description: .value | capture("SC\\d{4}\\]\\(.*?\\) (?<x>.+)$") | .x,
+              body: ("# shellcheck ${1|disable,enable|}=" + (.value | capture("^- +\\[(?<x>SC\\d{4})\\]") | .x))
+            }
+        }
+      )'
+}
+
+snippet_path=../snippets/snippets.json
+#jq '$ARGS.named["generated"] + $ARGS.named["source"]' --argjson generated "$(generate_snippets)" --argjson source "$(cat "$snippet_path")"
