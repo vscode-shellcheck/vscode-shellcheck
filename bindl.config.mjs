de4f16e1462e6a1f19ce45834a928048ae903b22
Felipe Santos <felipecassiors@gmail.com>
2025-01-12 16:07:54 -0300

    build(deps): bundle license from shellcheck binary as is (#1561)

--- bindl.config.mjs
+++ bindl.config.mjs
@@ -17,43 +17,36 @@ export default {
       platform: "linux",
       arch: "x64",
       url: `${releaseUrl}.linux.x86_64.tar.gz`,
-      files: [{ source: "shellcheck", target: "shellcheck" }],
     },
     {
       platform: "linux",
       arch: "arm",
       url: `${releaseUrl}.linux.armv6hf.tar.gz`,
-      files: [{ source: "shellcheck", target: "shellcheck" }],
     },
     {
       platform: "linux",
       arch: "arm64",
       url: `${releaseUrl}.linux.aarch64.tar.gz`,
-      files: [{ source: "shellcheck", target: "shellcheck" }],
     },
     {
       platform: "darwin",
       arch: "x64",
       url: `${releaseUrl}.darwin.x86_64.tar.gz`,
-      files: [{ source: "shellcheck", target: "shellcheck" }],
     },
     {
       platform: "darwin",
       arch: "arm64",
       url: `${releaseUrl}.darwin.aarch64.tar.gz`,
-      files: [{ source: "shellcheck", target: "shellcheck" }],
     },
     {
       platform: "win32",
       arch: "x64",
       url: `${releaseUrl}.windows.x86_64.tar.gz`,
-      files: [{ source: "shellcheck.exe", target: "shellcheck.exe" }],
     },
     {
       platform: "win32",
       arch: "arm64",
       url: `${releaseUrl}.windows.x86_64.tar.gz`,
-      files: [{ source: "shellcheck.exe", target: "shellcheck.exe" }],
     },
   ],
 };
