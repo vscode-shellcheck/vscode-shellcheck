18f5f0e4dc9af65f10bf7f62405cab5ec9bf95e6
Felipe Santos <felipecassiors@gmail.com>
2025-07-14 12:32:39 -0300

    build(deps-dev): download binaries directly from shellcheck releases

--- bindl.config.mjs
+++ bindl.config.mjs
@@ -1,44 +1,49 @@
 // @ts-check
 
 const version = `v0.10.0`;
-const releaseUrl = `https://github.com/vscode-shellcheck/shellcheck-binaries/releases/download/${version}/shellcheck-${version}`;
+const releaseUrl = `https://github.com/koalaman/shellcheck/releases/download/${version}/shellcheck-${version}`;
 
 export default {
   binaries: [
     {
       platform: "linux",
       arch: "x64",
-      url: `${releaseUrl}.linux.x86_64.tar.gz`,
+      url: `${releaseUrl}.linux.x86_64.tar.xz`,
+      stripComponents: 1,
     },
     {
       platform: "linux",
       arch: "arm",
-      url: `${releaseUrl}.linux.armv6hf.tar.gz`,
+      url: `${releaseUrl}.linux.armv6hf.tar.xz`,
+      stripComponents: 1,
     },
     {
       platform: "linux",
       arch: "arm64",
-      url: `${releaseUrl}.linux.aarch64.tar.gz`,
+      url: `${releaseUrl}.linux.aarch64.tar.xz`,
+      stripComponents: 1,
     },
     {
       platform: "darwin",
       arch: "x64",
-      url: `${releaseUrl}.darwin.x86_64.tar.gz`,
+      url: `${releaseUrl}.darwin.x86_64.tar.xz`,
+      stripComponents: 1,
     },
     {
       platform: "darwin",
       arch: "arm64",
-      url: `${releaseUrl}.darwin.aarch64.tar.gz`,
+      url: `${releaseUrl}.darwin.aarch64.tar.xz`,
+      stripComponents: 1,
     },
     {
       platform: "win32",
       arch: "x64",
-      url: `${releaseUrl}.windows.x86_64.tar.gz`,
+      url: `${releaseUrl}.zip`,
     },
     {
       platform: "win32",
       arch: "arm64",
-      url: `${releaseUrl}.windows.x86_64.tar.gz`,
+      url: `${releaseUrl}.zip`,
     },
   ],
 };
