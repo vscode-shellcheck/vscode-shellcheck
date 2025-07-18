1ef19a436723b68622a032db6e63e6077b939e08
Felipe Santos <felipecassiors@gmail.com>
2025-07-18 14:01:05 -0300

    chore: use `defineConfig` in bindl config (#1657)

--- bindl.config.mjs
+++ bindl.config.mjs
@@ -1,49 +1,70 @@
 // @ts-check
 
-const version = `v0.10.0`;
-const releaseUrl = `https://github.com/koalaman/shellcheck/releases/download/${version}/shellcheck-${version}`;
+import { defineConfig } from "bindl";
 
-export default {
+const version = "0.10.0";
+const releaseUrl = `https://github.com/koalaman/shellcheck/releases/download/v${version}/shellcheck-v${version}`;
+
+/** @type {import("bindl").BindlBinaryTest} */
+const unixTest = {
+  command: "./shellcheck --version",
+  expectedOutputContains: `version: ${version}`,
+};
+
+/** @type {import("bindl").BindlBinaryTest} */
+const windowsTest = {
+  command: ".\\shellcheck.exe --version",
+  expectedOutputContains: `version: ${version}`,
+};
+
+export default defineConfig({
   binaries: [
     {
       platform: "linux",
       arch: "x64",
       url: `${releaseUrl}.linux.x86_64.tar.xz`,
       stripComponents: 1,
+      tests: [unixTest],
     },
     {
       platform: "linux",
       arch: "arm",
       url: `${releaseUrl}.linux.armv6hf.tar.xz`,
       stripComponents: 1,
+      tests: [unixTest],
     },
     {
       platform: "linux",
       arch: "arm64",
       url: `${releaseUrl}.linux.aarch64.tar.xz`,
       stripComponents: 1,
+      tests: [unixTest],
     },
     {
       platform: "darwin",
       arch: "x64",
       url: `${releaseUrl}.darwin.x86_64.tar.xz`,
       stripComponents: 1,
+      tests: [unixTest],
     },
     {
       platform: "darwin",
       arch: "arm64",
       url: `${releaseUrl}.darwin.aarch64.tar.xz`,
       stripComponents: 1,
+      tests: [unixTest],
     },
     {
       platform: "win32",
       arch: "x64",
       url: `${releaseUrl}.zip`,
+      tests: [windowsTest],
     },
     {
       platform: "win32",
       arch: "arm64",
       url: `${releaseUrl}.zip`,
+      tests: [windowsTest],
     },
   ],
-};
+});
