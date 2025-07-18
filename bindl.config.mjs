97ae68ae48ff89c796a8ad125b2c4815f8dc0ba1
Felipe Santos <felipecassiors@gmail.com>
2025-07-18 13:58:32 -0300

    add bindl tests

--- bindl.config.mjs
+++ bindl.config.mjs
@@ -2,8 +2,20 @@
 
 import { defineConfig } from "bindl";
 
-const version = `v0.10.0`;
-const releaseUrl = `https://github.com/koalaman/shellcheck/releases/download/${version}/shellcheck-${version}`;
+const version = "0.10.0";
+const releaseUrl = `https://github.com/koalaman/shellcheck/releases/download/v${version}/shellcheck-v${version}`;
+
+/** @type {import("bindl").BindlBinaryTest} */
+const unixTest = {
+  command: "./shellcheck --version",
+  expectedOutputContains: `version: ${version}`,
+};
+
+/** @type {import("bindl").BindlBinaryTest} */
+const windowsTest = {
+  command: ".\\shellcheck.exe --version",
+  expectedOutputContains: `version: ${version}`,
+};
 
 export default defineConfig({
   binaries: [
@@ -12,40 +24,47 @@ export default defineConfig({
       arch: "x64",
       url: `${releaseUrl}.linux.x86_64.tar.xz`,
       stripComponents: 1,
+      tests: [unixTest],
     },
     {
       platform: "linux",
       arch: "arm",
       url: `${releaseUrl}.linux.armv6hf.tar.xz`,
       stripComponents: 1,
+      tests: [unixTest],
     },
     {
       platform: "linux",
       arch: "arm64",
       url: `${releaseUrl}.linux.aarch64.tar.xz`,
       stripComponents: 1,
+      tests: [unixTest],
     },
     {
       platform: "darwin",
       arch: "x64",
       url: `${releaseUrl}.darwin.x86_64.tar.xz`,
       stripComponents: 1,
+      tests: [unixTest],
     },
     {
       platform: "darwin",
       arch: "arm64",
       url: `${releaseUrl}.darwin.aarch64.tar.xz`,
       stripComponents: 1,
+      tests: [unixTest],
     },
     {
       platform: "win32",
       arch: "x64",
       url: `${releaseUrl}.zip`,
+      tests: [windowsTest],
     },
     {
       platform: "win32",
       arch: "arm64",
       url: `${releaseUrl}.zip`,
+      tests: [windowsTest],
     },
   ],
 });
